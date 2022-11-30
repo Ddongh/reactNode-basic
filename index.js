832950675ed38ccca9d98e2df81f3d68cf59a82c
pdh <qwer5383@naver.com>
2022-11-30 23:00:45 +0900

    #3 몽고DB연결

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://dong:1234@boilerplate.4k1r6l8.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send("Hello world~~~"))
 app.listen(port, () => console.log("Example app listening on port ${port}!"))
+
+
