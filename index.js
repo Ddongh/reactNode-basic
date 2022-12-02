a3181e0383389024a0e3e469a8710b6d140c5664
pdh <qwer5383@naver.com>
2022-12-02 23:06:53 +0900

    BodyParser & PostMan

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
+const { User } = require("./models/User");
+const bodyParser = require('body-parser');
+
+// application/s-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-
 mongoose.connect('mongodb+srv://dong:1234@boilerplate.4k1r6l8.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err)) 
 
 app.get('/', (req, res) => res.send("Hello world~~~"))
+app.post('/register', (req, res) => {
+    //화원 가입할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success : false, err})
+        return res.status(200).json({
+            success : true
+        })
+    })
+})
 app.listen(port, () => console.log("Example app listening on port ${port}!"))
 
 
