29eff453921ee37e6f186a261d628bf1e7c88eac
pdh <qwer5383@naver.com>
2022-12-06 22:40:29 +0900

    Bcrypt로 비밀번호 암호화하기

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err)) 
 
 app.get('/', (req, res) => res.send("Hello world~~~##"))
+ 
 app.post('/register', (req, res) => {
     //화원 가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body)
+
     user.save((err, userInfo) => {
         if(err) return res.json({ success : false, err})
         return res.status(200).json({
