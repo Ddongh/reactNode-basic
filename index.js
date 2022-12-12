28e386c0479a43b649f0f9282ddb29da84005235
pdh <qwer5383@naver.com>
2022-12-12 20:53:32 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { User } = require("./models/User");
 const bodyParser = require('body-parser');
 const config = require('./config/key');
 const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 
 // application/s-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended : true}));
@@ -21,8 +22,7 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err))  
 
 app.get('/', (req, res) => res.send("Hello world~~~##"))
-app.get('/1', (req, res) => res.send("Hello world~~~##"))
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     //화원 가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body)
@@ -35,7 +35,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => { 
+app.post('/api/users/login', (req, res) => { 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email : req.body.email }, (err, user) => {
         if(!user) {
@@ -47,26 +47,36 @@ app.post('/login', (req, res) => {
 
         //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
         user.comparePassword(req.body.password, (err, isMach) => {
-        if(!isMach)
-        return res.json({loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+            if(!isMach)
+            return res.json({loginSuccess : false, message : "비밀번호가 틀렸습니다."})
 
-        //비밀번호까지 맞다면 토큰을 생성하기
-        user.generateToken((err, user) => {
-            if(err) return res.status(400).send(err);
-            //토큰을 저장한다.(쿠기, 로컬 , ...)
-            res.cookie("x_auth", user.token)
-            .status(200)
-            .json({loginSuccess : true, userId : user._id})  
+            //비밀번호까지 맞다면 토큰을 생성하기
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                //토큰을 저장한다.(쿠기, 로컬 , ...)
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess : true, userId : user._id})  
 
+            })
         })
+    })    
+})
+
+// role 1 어드민        role 2 특정부서 어드민
+// role 0 일반유저       role 0이 아니면 관리자
+app.get('api/users/auth', auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+    res.status(200).json({
+        _id : req.user._id,
+        isAdmin : req.user.role === 0 ? false : true,
+        isAuth : true,
+        email : req.user.email,
+        name : req.user.name,
+        lastname : req.user.lastname,
+        role : req.user.role,
+        image : req.user.image     
     })
-
-
-    })
-
-    
-
-    
 })
 
 
