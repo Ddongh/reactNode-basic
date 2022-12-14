54ef21a8f91756e1c60e417f02613641d08dbe49
pdh <qwer5383@naver.com>
2022-12-14 19:29:30 +0900

    로그아웃기능

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ let auth = (req, res, next) => {
     //토큰을 복호화한 후 유저를 찾는다.
     User.findByToken(token, (err, user) => {
         if(err) throw err;
-        if(!err) return res.json({ isAuth : false, error : trur })
+        if(!user) return res.json({ isAuth : false, error : true })
 
         req.token = token;
         req.user = user;
