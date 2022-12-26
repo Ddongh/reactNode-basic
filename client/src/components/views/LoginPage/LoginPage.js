4a3af922675245f5c41e282a805c7734f7313da2
pdh <qwer5383@naver.com>
2022-12-26 23:07:21 +0900

    로그인 페이지(1)

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHander =(event => {
+        setPassword(event.currentTarget.value)
+    })
+
+    const onSubmitHander = (event) => {
+        alert("로그인버튼")
+    }
+
     return (
-        <div>
-            LoginPage
+        <div 
+        style={{display:'flex', justifyContent:'center', alignItems:'center'
+        , width:'100%', height:'100vh'
+        }}>
+            <form 
+            style={{display:'flex', flexDirection:'column'}}
+            onSubmit={onSubmitHander}
+            >
+                <label>Email</label>
+                <input type="eamil" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHander} />
+
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+
+            </form>
         </div>
     )
 }
