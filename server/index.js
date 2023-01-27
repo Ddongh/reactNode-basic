05f66846fa45fe544a58043f206c01528fdeca5f
pdh <qwer5383@naver.com>
2023-01-27 20:35:15 +0900

    인증체크(1)_3

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ app.post('/api/users/login', (req, res) => {
 
 // role 1 어드민        role 2 특정부서 어드민
 // role 0 일반유저       role 0이 아니면 관리자
-app.get('api/users/auth', auth, (req, res) => {
+app.get('api/users/auth', auth, (req, res) => {debugger;
     //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
     res.status(200).json({
         _id : req.user._id,
